8bd8d7baa0d7cc88fad9671738e5d8e12f2ea880
Sheng Dong <shengdong@MacBook-Pro-2.local>
2019-01-09 11:20:13 +1100

    Add all info

--- medical-form.js
+++ medical-form.js
@@ -9,23 +9,124 @@ $(document).ready(function() {
   });
 
   $('.next-step').click(function(e) {
+    if (!$('#medical-history-form').valid()) {
+      //I added an extra parenthesis at the end
+      return false;
+    }
+
+    // Add patient value to cosent raido button label patient_name
+    var $patient =
+      $('input[name="patient_title"]').val() +
+      ' ' +
+      $('input[name="first_name"]').val() +
+      ' ' +
+      $('input[name="last_name"]').val();
+    $('#patient_name')
+      .contents()
+      .last()
+      .replaceWith($patient);
+
+    // Navigate to next page
     var $active = $('.wizard .nav-tabs li.active');
     $active.next().removeClass('disabled');
     nextTab($active);
   });
+
   $('.prev-step').click(function(e) {
+    // Navigate to next page
     var $active = $('.wizard .nav-tabs li.active');
     prevTab($active);
   });
-  // Validating All Input And Textarea Fields
-  $('.submit_btn').click(function(e) {
-    if ($('input').val() == '' || $('textarea').val() == '') {
-      alert('*All Fields are mandatory*');
-      return false;
+
+  // check if yes raido button checked
+  $('.check-yes').click(function() {
+    $detail_input = $(this)
+      .parentsUntil('.block')
+      .siblings('.detail');
+
+    if ($(this).is(':checked')) {
+      $detail_input.find('input').attr('disabled', false);
+      $detail_input.find('tetarea').attr('disabled', false);
+      $detail_input.removeClass('disabled');
     } else {
-      return true;
+      $detail_input.find('input').attr('disabled', true);
+      $detail_input.find('tetarea').attr('disabled', true);
+      $detail_input.addClass('disabled');
     }
   });
+  // check if no raido button checked
+  $('.check-no').click(function() {
+    $detail_input = $(this)
+      .parentsUntil('.block')
+      .siblings('.detail');
+
+    if ($(this).is(':checked')) {
+      $detail_input.find('input').attr('disabled', true);
+      $detail_input.find('tetarea').attr('disabled', true);
+      $detail_input.addClass('disabled');
+    } else {
+      $detail_input.find('input').attr('disabled', false);
+      $detail_input.find('tetarea').attr('disabled', false);
+      $detail_input.removeClass('disabled');
+    }
+  });
+
+  //validate rules
+  /*
+  $('#medical-history-form').validate({
+    rules: {
+      first_name: {
+        required: true,
+        minlength: 3
+      },
+      last_name: {
+        required: true,
+        minlength: 3
+      },
+      phone: {
+        required: true,
+        number: true,
+        minlength: 3
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      gender: {
+        required: true
+      },
+      address_number: {
+        required: true
+      },
+      address_street_name: {
+        required: true
+      },
+      address_suburb: {
+        required: true
+      },
+      address_state: {
+        required: true
+      },
+      card_number: {
+        required: true,
+        number: true
+      },
+      card_reference: {
+        required: true
+      }
+    },
+    messages: {
+      gender: {
+        required: 'You must check at least 1 box'
+      }
+    },
+    hightlight: function(element) {
+      $(element)
+        .closest('.row-contact-details')
+        .addClass('error');
+    }
+  });
+  */
 });
 function nextTab(elem) {
   $(elem)
