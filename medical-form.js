baf48e585d8f0e33113dae2efa33a7fde1d4156b
Sheng Dong <shengdong@MacBook-Pro-2.local>
2019-01-09 15:17:16 +1100

    Added post function and demo php

--- medical-form.js
+++ medical-form.js
@@ -16,15 +16,23 @@ $(document).ready(function() {
 
     // Add patient value to cosent raido button label patient_name
     var $patient =
-      $('input[name="patient_title"]').val() +
+      $('input[name="title"]').val() +
       ' ' +
       $('input[name="first_name"]').val() +
       ' ' +
       $('input[name="last_name"]').val();
-    $('#patient_name')
-      .contents()
-      .last()
-      .replaceWith($patient);
+
+    if ($patient == '  ') {
+      $('#patient_name')
+        .contents()
+        .last()
+        .replaceWith('Thank you');
+    } else {
+      $('#patient_name')
+        .contents()
+        .last()
+        .replaceWith($patient);
+    }
 
     // Navigate to next page
     var $active = $('.wizard .nav-tabs li.active');
@@ -38,59 +46,97 @@ $(document).ready(function() {
     prevTab($active);
   });
 
+  /*
+   * Submit form with serialized form array
+   *
+   *
+   */
+  $('#submit-button').click(function(e) {
+    $.post(
+      '/medical-form/post.php',
+      {
+        name: $('form#medical-history-form').serializeArray()
+      },
+      function(output) {
+        console.log(output['result']);
+      },
+      'json'
+    );
+    e.preventDefault();
+  });
+
   // check if yes raido button checked
   $('.check-yes').click(function() {
     $detail_input = $(this)
-      .parentsUntil('.block')
+      .parentsUntil('.block-with-detail')
       .siblings('.detail');
 
     if ($(this).is(':checked')) {
       $detail_input.find('input').attr('disabled', false);
-      $detail_input.find('tetarea').attr('disabled', false);
+      $detail_input.find('textarea').attr('disabled', false);
       $detail_input.removeClass('disabled');
     } else {
       $detail_input.find('input').attr('disabled', true);
-      $detail_input.find('tetarea').attr('disabled', true);
+      $detail_input.find('textarea').attr('disabled', true);
       $detail_input.addClass('disabled');
     }
   });
   // check if no raido button checked
   $('.check-no').click(function() {
     $detail_input = $(this)
-      .parentsUntil('.block')
+      .parentsUntil('.block-with-detail')
       .siblings('.detail');
 
     if ($(this).is(':checked')) {
       $detail_input.find('input').attr('disabled', true);
-      $detail_input.find('tetarea').attr('disabled', true);
+      $detail_input.find('textarea').attr('disabled', true);
       $detail_input.addClass('disabled');
     } else {
       $detail_input.find('input').attr('disabled', false);
-      $detail_input.find('tetarea').attr('disabled', false);
+      $detail_input.find('textarea').attr('disabled', false);
       $detail_input.removeClass('disabled');
     }
   });
+
+  // Date picker
+  $('.input-group.date')
+    .datepicker()
+    .on('changeDate', function(ev) {
+      var newDate = new Date(ev.date);
+
+      $(this)
+        .siblings('.input-date-day')
+        .val(('0' + newDate.getDate()).slice(-2));
+      $(this)
+        .siblings('.input-date-month')
+        .val(('0' + (newDate.getMonth() + 1)).slice(-2));
+      $(this)
+        .siblings('.input-date-year')
+        .val(newDate.getFullYear());
+    });
 
   //validate rules
   /*
   $('#medical-history-form').validate({
     rules: {
-      first_name: {
-        required: true,
-        minlength: 3
-      },
-      last_name: {
-        required: true,
-        minlength: 3
-      },
-      phone: {
+      date_of_birth_day: {
         required: true,
         number: true,
-        minlength: 3
+        exactlength: 2,
+        min: 1,
+        max: 31
       },
-      email: {
+      date_of_birth_month: {
         required: true,
-        email: true
+        number: true,
+        exactlength: 2,
+        min: 1,
+        max: 12
+      },
+      date_of_birth_year: {
+        required: true,
+        number: true,
+        exactlength: 4
       },
       gender: {
         required: true
@@ -107,17 +153,17 @@ $(document).ready(function() {
       address_state: {
         required: true
       },
-      card_number: {
+      medicare_card_number: {
         required: true,
         number: true
       },
-      card_reference: {
+      medicare_card_reference: {
         required: true
       }
     },
     messages: {
       gender: {
-        required: 'You must check at least 1 box'
+        required: 'Please select at least 1 box'
       }
     },
     hightlight: function(element) {
@@ -140,3 +186,24 @@ function prevTab(elem) {
     .find('a[data-toggle="tab"]')
     .click();
 }
+// Added exacelength check
+$.validator.addMethod(
+  'exactlength',
+  function(value, element, param) {
+    if (this.optional(element)) {
+      return true;
+    }
+    if ($.isArray(param)) {
+      return $.inArray(value.length, param) != -1;
+    } else {
+      return value.length == param;
+    }
+  },
+  function(param, input) {
+    return $.validator.format(
+      'Please enter exactly ' +
+        ($.isArray(param) ? param.join() : param) +
+        ' characters.'
+    );
+  }
+);
